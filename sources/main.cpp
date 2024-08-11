b50dc8622dd632f9f0a9adebe55c0057f92b2a5b
Oleg <olezhanus@inbox.ru>
2024-08-11 23:30:05 +0300

    fixing changes

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,13 +1,27 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
-#include <vector>
-#include <iostream>
 #include <QApplication>
-#include "Program.h"
+#include <QTranslator>
+#include "mainwindow.h"
+#include <iostream>
+#include <vector>
+
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator myappTranslator;
+    myappTranslator.load("translations/my_ru.qm");
+    a.installTranslator(&myappTranslator);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("translations/qt_ru.qm");
+    a.installTranslator(&qtTranslator);
+
+    auto w = std::make_unique<MainWindow>();
+
+    w->show();
 
     return a.exec();
 }
